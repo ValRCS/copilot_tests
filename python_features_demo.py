0755c75f629fef16e59cdad79589a886b874266a
Valdis Saulespurēns <valdis.saulespurens@lnb.lv>
2023-07-13 13:21:06 +0300

    outline of python features

---
+++ python_features_demo.py
@@ -0,0 +1,154 @@
+# We would like to write a short demonstration of the features of Python
+
+# 1. Variables
+# 2. Data types
+# 3. Operators
+# 4. Control structures
+# 5. Functions
+# 6. Classes
+# 7. Modules
+# 8. Packages
+# 9. Exceptions
+# 10. File handling
+# 11. Regular expressions
+# 12. JSON
+# 13. Date and time
+# 14. Math
+# 15. Random
+# 16. Statistics
+# 17. Collections
+# 18. Itertools
+# 19. Threading
+# 20. Multiprocessing
+
+# 1. Variables
+# Variables are used to store data in memory
+# Variables can be of any type
+# Variables can be assigned to other variables
+# Variables can be reassigned to other values
+# Variables can be reassigned to other types
+
+# 2. Data types
+# Python has several built-in data types
+# int, float, str, bool, list, tuple, dict, set, frozenset, bytes, bytearray, memoryview, complex, range, None
+# We can check the type of a variable using the type() function
+# We can convert between types using the int(), float(), str(), bool(), list(), tuple(), dict(), set(), frozenset(), bytes(), bytearray(), memoryview(), complex(), range(), None() functions
+
+# 3. Operators
+# Python has several built-in operators
+# Arithmetic operators: +, -, *, /, //, %, **
+# Assignment operators: =, +=, -=, *=, /=, //=, %=, **=
+# Comparison operators: ==, !=, >, <, >=, <=
+# Logical operators: and, or, not
+# Identity operators: is, is not
+# Membership operators: in, not in
+# Bitwise operators: &, |, ^, ~, <<, >>
+
+# 4. Control structures
+# Python has several built-in control structures
+# if, elif, else
+# for, while
+# break, continue, pass
+
+# 5. Functions
+# Functions are used to group code that performs a specific task
+# Functions can be called from other functions
+# Functions can be called from other files
+# Functions can be passed arguments
+# Functions can return values
+
+# 6. Classes
+# Classes are used to group data and functions that perform a specific task
+# Classes can be instantiated
+# Classes can be inherited
+# Classes can be passed arguments
+# Classes can return values
+
+# 7. Modules
+# Modules are used to group code that performs a specific task
+# Modules can be imported into other modules
+# Modules can be imported into other files
+# Modules can be passed arguments
+# Modules can return values
+
+# 8. Packages
+# Packages are used to group modules that perform a specific task
+# Packages can be imported into other modules
+# Packages can be imported into other files
+# Packages can be passed arguments
+# Packages can return values
+
+# 9. Exceptions
+# Exceptions are used to handle errors
+# Exceptions can be raised
+# Exceptions can be caught
+# Exceptions can be handled
+
+# 10. File handling
+# Files are used to store data on disk
+# Files can be opened
+# Files can be read
+# Files can be written
+# Files can be closed
+
+# 11. Regular expressions
+# Regular expressions are used to match patterns in strings
+# Regular expressions can be compiled
+# Regular expressions can be matched
+# Regular expressions can be searched
+# Regular expressions can be replaced
+
+# 12. JSON
+# JSON is used to store data in a human-readable format
+# JSON can be encoded
+# JSON can be decoded
+
+# 13. Date and time
+# Date and time are used to store dates and times
+# Date and time can be formatted
+# Date and time can be parsed
+
+# 14. Math
+# Math is used to perform mathematical operations
+# Math can be used to calculate the absolute value
+# Math can be used to calculate the ceiling
+# Math can be used to calculate the floor
+# Math can be used to calculate the factorial
+# Math can be used to calculate the greatest common divisor
+# Math can be used to calculate the least common multiple
+# Math can be used to calculate the logarithm
+# Math can be used to calculate the power
+# Math can be used to calculate the square root
+# Math can be used to calculate the trigonometric functions
+
+# 15. Random
+# Random is used to generate random numbers
+# Random can be used to generate random numbers
+
+# 16. Statistics
+# Statistics is used to perform statistical operations
+# Statistics can be used to calculate the mean
+# Statistics can be used to calculate the median
+# Statistics can be used to calculate the mode
+# Statistics can be used to calculate the standard deviation
+# Statistics can be used to calculate the variance
+
+# 17. Collections
+# Collections are used to store collections of data
+# Collections can be used to store collections of data
+
+# 18. Itertools
+# Itertools are used to iterate over collections of data
+# Itertools can be used to iterate over collections of data
+
+# 19. Threading
+# Threading is used to run multiple threads of execution
+# Threading can be used to run multiple threads of execution
+
+# 20. Multiprocessing
+# Multiprocessing is used to run multiple processes
+# Multiprocessing can be used to run multiple processes
+
+
+# 1. Variables
+
